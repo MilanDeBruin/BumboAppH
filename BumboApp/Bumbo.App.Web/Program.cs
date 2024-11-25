3dcb40f7ddfb89e4845afca38e0adc99d6064877
N1TROGUE <n1trogue@protonmail.com>
2024-11-25 17:19:30 +0100

    Scaffolded .NET Identity Views

--- BumboApp/Bumbo.App.Web/Program.cs
+++ BumboApp/Bumbo.App.Web/Program.cs
@@ -1,6 +1,7 @@
 using Bumbo.Data.Context;
 using Bumbo.Data.Models;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bumbo.App.Web
@@ -15,14 +16,17 @@ namespace Bumbo.App.Web
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<BumboDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("Bumbo")));
+
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<BumboDbContext>();
             
             builder.Services.AddTransient<Bumbo.Data.Interfaces.INormRepository, Bumbo.Data.SqlRepository.NormRepository>();
             builder.Services.AddTransient<Bumbo.Data.Interfaces.IForecastRepository, Bumbo.Data.SqlRepository.ForecastRepository>();
             builder.Services.AddTransient<Bumbo.Domain.Services.Forecast.IGenerateForecastService, Bumbo.Domain.Services.Forecast.GenerateForecastService>();
 
-
-
-
+            builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>(
+                    options => { options.SignIn.RequireConfirmedAccount = false; })
+                .AddEntityFrameworkStores<BumboDbContext>();
+ 
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
                 {
