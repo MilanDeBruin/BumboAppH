444cb7ce2fe84aacf2b6fadde48ad4ae3dcdb8f4
Sven Lempers <svenlempers@gmail.com>
2024-12-04 00:54:57 +0100

    finished cao schedule service and integrated completely with scheduling

--- BumboApp/Bumbo.App.Web/Program.cs
+++ BumboApp/Bumbo.App.Web/Program.cs
@@ -21,11 +21,13 @@ namespace Bumbo.App.Web
             builder.Services.AddTransient<Bumbo.Data.Interfaces.INormRepository, Bumbo.Data.SqlRepository.NormRepository>();
             builder.Services.AddTransient<Bumbo.Data.Interfaces.IForecastRepository, Bumbo.Data.SqlRepository.ForecastRepository>();
             builder.Services.AddTransient<Bumbo.Data.Interfaces.IAvailabilityRepository, Bumbo.Data.SqlRepository.AvailabilityRepository>();
+            builder.Services.AddTransient<Bumbo.Data.Interfaces.IScheduleRepository, Bumbo.Data.SqlRepository.ScheduleRepository>();
+            builder.Services.AddTransient<Bumbo.Data.Interfaces.ICaoRepository, Bumbo.Data.SqlRepository.CaoRepository>();
             builder.Services.AddTransient<Bumbo.Domain.Services.Forecast.IGenerateForecastService, Bumbo.Domain.Services.Forecast.GenerateForecastService>();
             builder.Services.AddTransient<Bumbo.Domain.Services.Leaves.ILeaveChecker, Bumbo.Domain.Services.Leaves.LeaveChecker>();
             builder.Services
                 .AddTransient<Bumbo.Domain.Services.CAO.ICaoScheduleService,
-                    Bumbo.Domain.Services.CAO.CaoScheduleServiceStandin>();
+                    Bumbo.Domain.Services.CAO.CaoScheduleService>();
 
 
 
