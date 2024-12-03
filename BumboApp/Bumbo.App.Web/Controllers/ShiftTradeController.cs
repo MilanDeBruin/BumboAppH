70cb6658b7ae2d9388ca934e6c975fe2341d641d
LFweide <lf.vanderweide@student.avans.nl>
2024-12-03 13:46:41 +0100

    start aan shift trading

---
+++ BumboApp/Bumbo.App.Web/Controllers/ShiftTradeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bumbo.App.Web.Controllers
+{
+    public class ShiftTradeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
