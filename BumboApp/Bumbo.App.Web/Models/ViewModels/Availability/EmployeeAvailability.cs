672103564f284894d621a32c4e36846a60f19e88
MilanDeBruin <milandebruin03@gmail.com>
2024-12-03 12:36:05 +0100

    fixed merge conflicts

--- BumboApp/Bumbo.App.Web/Models/ViewModels/Availability/EmployeeAvailability.cs
+++ BumboApp/Bumbo.App.Web/Models/ViewModels/Availability/EmployeeAvailability.cs
@@ -4,7 +4,7 @@ namespace Bumbo.App.Web.Models.ViewModels.Availability;
 
 public class EmployeeAvailability
 {
-    public EmployeeModel Employee { get; set; }
+    public EmployeeViewModel Employee { get; set; }
     public TimeOnly? StartTime { get; set; }
     public TimeOnly? EndTime { get; set; }
 }
