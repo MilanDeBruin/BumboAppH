27862146bf9ec0dc2e6d610d628b67d4785b2425
chang-wen-jie <changwenjie1999@gmail.com>
2024-12-06 21:57:30 +0100

    fix weekday order and availability layout

--- BumboApp/Bumbo.App.Web/Models/ViewModels/Availability/AvailabilityViewModel.cs
+++ BumboApp/Bumbo.App.Web/Models/ViewModels/Availability/AvailabilityViewModel.cs
@@ -1,16 +1,14 @@
-﻿namespace Bumbo.App.Web.Models.ViewModels.Availability
-{
-    public class AvailabilityViewModel
-    {
-        public int EmployeeId { get; set; }
-        public List<DailyAvailability> DailyAvailabilities { get; set; } = new List<DailyAvailability>();
-        public List<string> Weekdays { get; set; } = new List<string>();
-    }
+﻿namespace Bumbo.App.Web.Models.ViewModels.Availability;
 
-    public class DailyAvailability
-    {
-        public string Weekday { get; set; } = null!;
-        public TimeOnly? StartTime { get; set; }
-        public TimeOnly? EndTime { get; set; }
-    }
+public class AvailabilityViewModel
+{
+    public int EmployeeId { get; set; }
+    public List<DailyAvailability> DailyAvailabilities { get; set; } = new List<DailyAvailability>();
+}
+
+public class DailyAvailability
+{
+    public DateOnly Weekday { get; set; }
+    public TimeOnly? StartTime { get; set; }
+    public TimeOnly? EndTime { get; set; }
 }
