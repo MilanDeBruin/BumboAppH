86a4df04aa855391def5af661bd9469cd44dc1bb
Gabriel Matos <gabriel@capitola.nl>
2025-01-08 17:03:56 +0100

    dag overzicht eerste versie

---
+++ BumboApp/Bumbo.App.Web/Models/ViewModels/Dayoverview/DayOverviewViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Bumbo.App.Web.Models.ViewModels.Dayoverview
+{
+	public class DayOverviewViewModel
+	{
+		public int EmployeeId { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public Decimal PlannedHours { get; set; }
+		public Decimal WorkedHours { get; set; }
+		public Decimal Difference => PlannedHours - WorkedHours;
+		public int BranchId { get; set; }
+	}
+}
