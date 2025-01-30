00616b4d1a7e196cf72250278d774596f335327b
LFweide <lf.vanderweide@student.avans.nl>
2025-01-30 12:20:58 +0100

    de shifts die iemand werkt kan alleen worden opgeslagen als het langer dan een kwartier is en alle tijd worden of gezet naar het dichtbijzijnde kwartier

--- BumboApp/Bumbo.App.Web/Controllers/HomeController.cs
+++ BumboApp/Bumbo.App.Web/Controllers/HomeController.cs
@@ -19,7 +19,7 @@ namespace Bumbo.App.Web.Controllers
     public class HomeController : Controller
     {
         private readonly IHomeRepository _repo;
-        
+
         public HomeController(IHomeRepository repo)
         {
             _repo = repo;
@@ -38,37 +38,37 @@ namespace Bumbo.App.Web.Controllers
             }
 
             var viewModel = new WeekPersonalScheduleViewModel
-                {
-                    FirstDayOfWeek = firstDayOfWeek,
-                    WorkDays = new List<DayPersonalScheduleViewModel>()
-                };
-                List<WorkSchedule> schedules = _repo.GetScheduleData(employeeId, firstDayOfWeek);
-                
-                 var groupedSchedules = schedules
-                .GroupBy(schedule => schedule.Date)
-                .OrderBy(group => group.Key);
+            {
+                FirstDayOfWeek = firstDayOfWeek,
+                WorkDays = new List<DayPersonalScheduleViewModel>()
+            };
+            List<WorkSchedule> schedules = _repo.GetScheduleData(employeeId, firstDayOfWeek);
 
-                foreach (var group in groupedSchedules)
+            var groupedSchedules = schedules
+           .GroupBy(schedule => schedule.Date)
+           .OrderBy(group => group.Key);
+
+            foreach (var group in groupedSchedules)
+            {
+                var daySchedule = new DayPersonalScheduleViewModel
                 {
-                    var daySchedule = new DayPersonalScheduleViewModel
+                    Date = group.Key,
+                    Shifts = group.Select(schedule => new ShiftsViewModel
                     {
-                        Date = group.Key,
-                        Shifts = group.Select(schedule => new ShiftsViewModel
-                        {
-                            Time = $"{schedule.StartTime:HH:mm} - {schedule.EndTime:HH:mm}".ToString(),
-                            Departement = schedule.Department,
-                            Branch_Id = schedule.BranchId,
-                            Is_Sick = schedule.IsSick
-                        }).ToList()
-                    };
-                    viewModel.WorkDays.Add(daySchedule);
+                        Time = $"{schedule.StartTime:HH:mm} - {schedule.EndTime:HH:mm}".ToString(),
+                        Departement = schedule.Department,
+                        Branch_Id = schedule.BranchId,
+                        Is_Sick = schedule.IsSick
+                    }).ToList()
+                };
+                viewModel.WorkDays.Add(daySchedule);
 
-                }
-                viewModel.ingeklokt = _repo.GetIngeklokt(employeeId);
-                viewModel.isSick = _repo.GetSick(employeeId);
-                viewModel.sickListNames = _repo.getSickList();
-                return View(viewModel);
-           
+            }
+            viewModel.ingeklokt = _repo.GetIngeklokt(employeeId);
+            viewModel.isSick = _repo.GetSick(employeeId);
+            viewModel.sickListNames = _repo.getSickList();
+            return View(viewModel);
+
         }
 
         [HttpGet]
@@ -92,19 +92,57 @@ namespace Bumbo.App.Web.Controllers
 
         public IActionResult Inklokken(int employeeId)
         {
-            if(_repo.GetSick(employeeId) == true)
+            var currentTime = DateTime.Now;
+            int minutes = currentTime.Minute;
+
+            int roundedMinutes = (int)(Math.Round(minutes / 15.0) * 15);
+            if (roundedMinutes == 60)
             {
-                TempData["WarningMessage"] = "Je bent ziek en mag niet inklokken!";
+                currentTime = currentTime.AddHours(1);
+                roundedMinutes = 0;
+            }
+
+            currentTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, roundedMinutes, 0);
+
+            //if(_repo.GetSick(employeeId) == true)
+            //{
+            //    TempData["WarningMessage"] = "Je bent ziek en mag niet inklokken!";
+            //    return RedirectToAction("Index");
+            //}
+            if (_repo.CheckStartTime(employeeId, currentTime))
+            {
+                TempData["WarningMessage"] = "Je bent al ingeklokt binnen dit kwartier!";
                 return RedirectToAction("Index");
             }
-            _repo.Inklokken(employeeId);
-            TempData["SuccessMessage"] = "Je bent ingeklokt!";
-            return RedirectToAction("Index");
+            else
+            {
+                _repo.Inklokken(employeeId, currentTime);
+                TempData["SuccessMessage"] = "Je bent ingeklokt!";
+                return RedirectToAction("Index");
+            }
+           
         }
 
         public IActionResult Uitklokken(int employeeId)
         {
-            _repo.Uitklokken(employeeId);
+            var currentTime = DateTime.Now;
+            int minutes = currentTime.Minute;
+
+            int roundedMinutes = (int)(Math.Round(minutes / 15.0) * 15);
+            if (roundedMinutes == 60)
+            {
+                currentTime = currentTime.AddHours(1);
+                roundedMinutes = 0;
+            }
+            currentTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, roundedMinutes, 0);
+
+            if (_repo.CheckStartTime(employeeId, currentTime))
+            {
+                TempData["WarningMessage"] = "Je shift was korter dan een kwartier en is niet opgeslagen";
+                _repo.DeleteShift(employeeId, currentTime);
+                return RedirectToAction("Index");
+            }
+            _repo.Uitklokken(employeeId, currentTime);
             TempData["SuccessMessage"] = "Je bent uitgeklokt!";
             return RedirectToAction("Index");
         }
